6f979feda8853c81e6e9c810da1af66c469c0341
Victor Anih <anihvc@yahoo.com>
2026-08-04 15:04:29 +0100

    fix: adjust environment variable loading for root .env architecture

--- server/index.js
+++ server/index.js
@@ -819,11 +819,11 @@ const server = http.createServer(async (request, response) => {
 
 
 
-const PORT = process.env.PORT || 5000; 
+//const PORT = process.env.PORT || 5000; 
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Backend server successfully running on port ${PORT}`);
-});
+//app.listen(PORT, '0.0.0.0', () => {
+  //console.log(`Backend server successfully running on port ${PORT}`);
+//});
 
 const express = require('express');
 const cors = require('cors');
