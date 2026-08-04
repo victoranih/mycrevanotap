002fba5eb9775e6a2f9e8e85b08d44583999c176
Victor Anih <anihvc@yahoo.com>
2026-08-04 17:40:06 +0100

    fix: purge remaining legacy commonJS require block from bottom of index

--- server/index.js
+++ server/index.js
@@ -860,7 +860,7 @@ const server = http.createServer(async (request, response) => {
 
 
 //const cors = require('cors');
-const path = require('path');
+//const path = require('path');
 
 // ONLY load dotenv if we are working locally. 
 // Render handles this automatically in production.
